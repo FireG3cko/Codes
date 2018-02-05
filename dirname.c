fd6e05e51bf62689a379ed3593b705cd8b543446
FireG3cko <fireg3cko@protonmail.com>
2018-02-05 10:15:33 -0500

    lol why do I feel the need to explain dirname.c

--- dirname.c
+++ dirname.c
@@ -1,3 +1,8 @@
+//Basic 'script' I once wrote to fix my music directory.
+//I downloaded my songs from my Google Drive backups, and all the apostrophes became underscores.
+//I'm sure this could be condensed to three lines of bash,
+//But I partly wrote this to learn more practical C.
+
 #include <stdio.h>
 #include <dirent.h>
 
